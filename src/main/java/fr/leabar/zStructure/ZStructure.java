0662eec6dd11dc82f058781091f577befd2f0c46
Léa BAR <contact@0xlea.fr>
2025-07-02 20:20:59 +0200

    [Init] Init project

---
+++ src/main/java/fr/leabar/zStructure/ZStructure.java
@@ -0,0 +1,17 @@
+package fr.leabar.zStructure;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZStructure extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
